0666164683ee05817336eea7c164f3e70821b14c
juan <ivandavid18299@gmail>
2022-06-02 16:43:47 -0300

    first commit

---
+++ .Temp~/dbD1SQL.sql
@@ -0,0 +1,10 @@
+UPDATE "APP"."APPLICATION" 
+SET refresh_token_validity = 2592000, 
+    access_token_validity = 3600
+WHERE app_id = 'unsa_cargos';
+
+UPDATE "APP"."APPLICATION" 
+SET refresh_token_validity = 2592000, 
+    access_token_validity = 3600
+WHERE app_id = 'unsa_personal';
+
